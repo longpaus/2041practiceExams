5a0fc4e88d54882dfbb9d4bc21f9d2848ff6c5ea
Hoang Long <longpaus@gmail.com>
2022-08-09 17:22:35 +1000

    fix q6

--- exampractice1/practice_q6.py
+++ exampractice1/practice_q6.py
@@ -3,6 +3,6 @@ import fileinput
 for line in fileinput.input():
     lineArr = line.split('|')
     arrangedName = ' '.join(lineArr[2].strip().split(',')[::-1]).ljust(33)
-    lineArr[2] = arrangedName
+    lineArr[2] = arrangedName[1:]
     print('|'.join(lineArr),end='')
          
