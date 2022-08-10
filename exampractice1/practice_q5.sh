9335728bf0ce38e41ccabcc490d340070ea6da68
Hoang Long <longpaus@gmail.com>
2022-08-10 20:31:32 +1000

    fix q5

--- exampractice1/practice_q5.sh
+++ exampractice1/practice_q5.sh
@@ -6,7 +6,7 @@ for file in log.6 log.5 log.4 log.3 log.2 log.1 log; do
             if [ ! -d archive ];then
                 mkdir archive
             fi
-            newName="$(date +'%Y_%m_%d').gz"
+            newName="log.$(date +'%Y_%m_%d').gz"
             gzip log.6
             mv log.6.gz $newName
             mv $newName archive/
